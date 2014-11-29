a0f1c649ece4fa4d47aa4250b577db49e321b390
Ari Porad <ari.porad+git@gmail.com>
2014-11-29 10:13:20 -0800

    Moved Client & Server tests to sperate directories

---
+++ test/mocha/server/tests.js
@@ -0,0 +1,26 @@
+// test/mocha/node-suite.js
+
+// This runs with mocha programmatically rather than from the command line.
+// how-to-with-comments taken from https://github.com/itaylor/qunit-mocha-ui
+
+
+//Load Code Coverage (Blanket)
+require('blanket')({
+    pattern: function (filename) {
+        return !/node_modules/.test(filename);
+    }
+});
+
+//Load mocha
+var Mocha = require("mocha");
+
+//Tell mocha to use the interface.
+var mocha = new Mocha({ui:"bdd", reporter:"mocha-multi"});
+
+//Add your test files
+mocha.addFile("./test/mocha/suite.js");
+
+//Run your tests
+mocha.run(function(failures){
+  process.exit(failures);
+});
