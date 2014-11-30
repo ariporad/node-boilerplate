f00d08e88d9627a1a778a03628305eb28469323b
Ari <ariporad@gmail.com>
2014-11-30 16:36:51 +0000

    Changed from sass to stylus

--- config/express.js
+++ config/express.js
@@ -2,8 +2,11 @@ var mustacheExpress = require("mustache-express");
 var utils = require(__dirname + "/lib/utils.js");
 var cookieParser = require("cookie-parser");
 var session = require("express-session");
+var stylus = require("stylus");
 
 module.exports = function (app, express, server) {
+	var isDev = process.env.NODE_ENV == 'development';
+	
 	app.engine('mustache', mustacheExpress());
 	
 	app.set('view engine', 'mustache');
@@ -13,6 +16,12 @@ module.exports = function (app, express, server) {
 	app.use(session({
 		secret: utils.generateSecret()
 		// TODO: Setup session store to database (Redis?)
+	}))
+	app.use(stylus.middleware({
+		src: __dirname + "/../src/stylus",
+		dest: __dirname + "/../public/css",
+		force: isDev,
+		linenos: isDev
 	}))
 	app.use(express.static("public"));
 	app.use(express.static("vendor"));
--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// main boilerplate
+var express = require("express");
 
-;(function () {
 
-  // node.js
-  if (typeof module != "undefined") {
-    module.exports = boilerplate;
-  }
-  // browser
-  if (typeof window != "undefined") {
-    !window.boilerplate && (window.boilerplate = boilerplate);
-  }
-
-  function boilerplate() {
-    return 'boilerplate';
-  }
-}());
