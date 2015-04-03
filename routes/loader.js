f7ef76a5ee2e1e82780d4c0db4c6aea928f964a7
Ari Porad <ari@porad.com>
2015-04-03 08:24:05 -0700

    Added some comments

--- routes/loader.js
+++ routes/loader.js
@@ -1,5 +1,7 @@
 module.exports = exports = function (app, express, server) {
 	var modules = {};
+	
+	// Walk this directory recrusivly, and make a list of all the module you find
 	utils.walk(__dirname + "/").forEach(function(file) {
 		if (file.split(".").last === "js" && file !== 'loader.js') {
 			var name = file.replace('.js', '');
@@ -7,12 +9,19 @@ module.exports = exports = function (app, express, server) {
 		}
 	});
 	
+	// For each one
 	for (var i = 0; i < modules.length; i++) {
 		var route = modules[i];
+		
+		// If it exports and init function, call it first
 		if (typeof modules[route].init === 'function') {
 			modules[route].init(app, express, server);
 		}
 		
+		// This is a bit complicated. I wanted to allow the modules to export other things as well,
+		// So what I did was to default to using exports[get/put/post/delete] as routes, and everything else as whatever.
+		// But then I deided to add something so that a module could use all of the other ~25 HTTP verbs express supports,
+		// So they can optionaly export a exports[verbs] array, which contains a list of verbs that they would like to be used as well
 		var verbs = utils.removeDuplicates(["get", "put", "post", "delete"].concat(modules[route].verbs));
 		for (var I = 0; I < verbs.length; I++) {
 			var verb = verbs[i];
