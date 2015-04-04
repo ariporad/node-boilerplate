d36b6fa293b305ed9a238fc0526f8599c05644c8
Ari Porad <ari@porad.com>
2015-04-04 08:21:08 -0700

    Fixed a tiny issue that gave me way too much headache

--- routes/loader.js
+++ routes/loader.js
@@ -1,38 +1,41 @@
 module.exports = exports = function (app, express, server, dir) {
-	var modules = {};
+	var modules = [];
 	dir = dir || __dirname;
 	
+	var walk = utils.walk;
+	
 	// Walk this directory recrusivly, and make a list of all the module you find
-	utils.walk(dir + "/").forEach(function(file) {
+	walk(dir + "/").forEach(function(file) {
 		if (file.split(".").last === "js" && file !== 'loader.js') {
 			var name = file.replace('.js', '');
-			modules[name] = require('./' + file);
+			modules.push({ route: name, module: require('./' + file) });
 		}
 	});
 	
 	// And now we sort them
 	modules.sort(function (a, b) {
-		return ( a.priority || 0 ) - ( b.priority || 0 );
+		return ( a.module.priority || 0 ) - ( b.module.priority || 0 );
 	});
 	
 	// For each one
 	for (var i = 0; i < modules.length; i++) {
-		var route = modules[i];
+		var mod   = modules[i].mod,
+			route = modules[i].route;
 		
 		// If it exports and init function, call it first
-		if (typeof modules[route].init === 'function') {
-			modules[route].init(app, express, server);
+		if (typeof mod.init === 'function') {
+			mod.init(app, express, server);
 		}
 		
 		// This is a bit complicated. I wanted to allow the modules to export other things as well,
 		// So what I did was to default to using exports[get/put/post/delete] as routes, and everything else as whatever.
 		// But then I deided to add something so that a module could use all of the other ~25 HTTP verbs express supports,
 		// So they can optionaly export a exports[verbs] array, which contains a list of verbs that they would like to be used as well
-		var verbs = utils.removeDuplicates(["get", "put", "post", "delete"].concat(modules[route].verbs));
+		var verbs = utils.removeDuplicates(["get", "put", "post", "delete"].concat(mod.verbs));
 		for (var I = 0; I < verbs.length; I++) {
 			var verb = verbs[i];
-			if (typeof route[verb] === 'function') {
-				app[verb](route, modules[route]);
+			if (typeof mod[verb] === 'function') {
+				app[verb](route, mod);
 			}
 		}
 	}
