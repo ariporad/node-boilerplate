aba8e81816ce5a2b1fef3557d911f2e3e0200b0d
Ari Porad <ari@porad.com>
2015-04-05 17:27:50 -0700

    Added test for utils.removeDuplicates()

--- test/mocha/node/utils.js
+++ test/mocha/node/utils.js
@@ -6,7 +6,7 @@ describe('utils', function(){
 		it('Should exist', function() {
 		    utils.should.have.property('generateSecret');
 		});
-		it('Should be a function.', function(){
+		it('Should be a function', function() {
 			utils.generateSecret.should.be.a('function');
 		});
 		it('Should return a string', function() {
@@ -21,7 +21,7 @@ describe('utils', function(){
 		it('Should exist', function() {
 		    utils.should.have.property('fail');
 		});
-		it('Should be a function.', function(){
+		it('Should be a function', function() {
 			utils.fail.should.be.a('function');
 		});
 		it('It should throw an error', function() {
@@ -31,4 +31,31 @@ describe('utils', function(){
 			(function() { utils.fail("Testing 1, 2, 3") }).should.throw(Error, "Testing 1, 2, 3");
 		});
 	});
+	
+	
+	describe('removeDuplicates()', function() {
+		var array 				= [1, 2, 3, 4, 5], 
+			arrayWithDuplicates = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
+		
+		it('Should exist', function() {
+		    utils.should.have.property('removeDuplicates');
+		});
+		it('Should be a function', function() {
+			utils.removeDuplicates.should.be.a('function');
+		});
+		
+		it('Should return an array', function() {
+			utils.removeDuplicates([1, 2, 2, 3]).should.be.an('array');
+		});
+		
+		it('Should remove the duplicates from an array', function() {
+			utils.removeDuplicates(arrayWithDuplicates).should.eql(array);
+		});
+		
+		it('Should not modify the originaly array', function () {
+			var original = arrayWithDuplicates.slice(0);
+			arrayWithDuplicates.should.eql(original);
+		});
+	});
 });
+
