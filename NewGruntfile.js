4dab8f4943a5e6a55a810103c7a123e251b03545
Ari Porad <ari@porad.com>
2015-06-24 20:18:02 -0700

    Add linting to new gruntfile [Not Working]

--- NewGruntfile.js
+++ NewGruntfile.js
@@ -20,7 +20,7 @@ module.exports = function (grunt) {
 				src: ['build']
 			},
 			stylesheets: {
-				src: ['build/**/*.css', '!build/bundle.css']
+				src: ['build/**/*.css', 'build/**/*.styl', '!build/bundle.css']
 			},
 			scripts: {
 				src: ['build/**/*.js', '!build/bundle.js']
@@ -90,6 +90,33 @@ module.exports = function (grunt) {
 				tasks: ['copy']
 			}
 		},
+
+		// Lint the JS files
+		jshint: {
+			options: {
+				lastsemic: true, // Allows function(){ return 'Something' } <-- Notice no ';' after return for single-line function
+				laxcomma: true, // Allows comma-first coding
+				proto: true, // Allows __proto__
+			},
+			node: {
+				files: {
+					src: ['src/**/*.js', 'Gruntfile.js', '!src/public/**', '!src/vendor/**', '!**.test.js', '!test.**']
+				},
+				options: {
+					node: true // Some Node.js specific stuff
+				}
+			},
+			client: {
+				files: {
+					src: ["src/public/js/**/*.js"]
+				},
+				options: {
+					browser: true, // navigator and stuff, plus HTML5 APIs
+					devel: true, // console.log and stuff
+					jquery: true // Obvious, jQuery stuff such as $
+				}
+			}
+		},
 	});
 
 	// load the tasks
@@ -100,7 +127,7 @@ module.exports = function (grunt) {
 
 	grunt.registerTask('scripts', 'Compiles the JavaScript files.', ['uglify', 'clean:scripts']);
 
-	grunt.registerTask('build', 'Compiles all of the assets and copies the files to the build directory.', ['clean:build', 'copy', 'stylesheets']);
+	grunt.registerTask('build', 'Compiles all of the assets and copies the files to the build directory.', ['clean:build', 'copy', 'stylesheets', 'scripts']);
 
-	grunt.registerTask('default', 'Watches the project for changes, automatically builds them and runs a server.', ['build', 'connect', 'watch']);
+	grunt.registerTask('default', 'Watches the project for changes, automatically builds them and runs a server.', ['build', 'watch']);
 };
