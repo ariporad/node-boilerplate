37f52ce2bf5ce0d8770a4e5089a7593c64928404
Ari Porad <ari@porad.com>
2015-07-11 08:47:10 -0700

    Converted main.test to es6

--- src/main.test.js
+++ src/main.test.js
@@ -1,12 +1,11 @@
-var main = require('./main');
-var assert =  require('assert');
+import * as main from 'main';
 
 describe('The Server', function() {
 	it('Should create an Express server.', function () {
 		it('Should import Express', function(){
 
 		});
-		//assert.equal('fail', 'boilerplate', 'Dud test.'); //TODO: Add real tests
-		// test
+		//assert.equal('fail', 'boilerplate', 'Dud test.'); //TODO: Add real
+		// tests test
 	});
 });
